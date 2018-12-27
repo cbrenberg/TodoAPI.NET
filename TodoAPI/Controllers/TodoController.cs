45fb5048a4414d2c43f3f8002152c8371648be4b
Christopher Brenberg <cbrenberg@gmail.com>
2018-12-27 12:18:24 -0600

    initial commit

---
+++ TodoAPI/Controllers/TodoController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TodoAPI.Controllers
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
---
+++ TodoAPI/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TodoAPI.Models
+{
+    public class TodoItem
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
